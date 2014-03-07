94b6783324063ad7c761a39c5f2de625e079313d
Jon Taylor <jontaylor@cpan.org>
2014-03-07 17:32:39 +0000

    Import of JONTAYLOR/DB-SQL-Migrations-0.06 from CPAN.
    gitpan-cpan-distribution: DB-SQL-Migrations
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         JONTAYLOR/DB-SQL-Migrations-0.06.zip
    gitpan-cpan-author:       JONTAYLOR
    gitpan-cpan-maturity:     released

--- lib/DB/SQL/Migrations.pm
+++ lib/DB/SQL/Migrations.pm
@@ -6,7 +6,7 @@ use File::Basename;
 use DBIx::MultiStatementDo;
 use File::Slurp;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 has [qw( dbh migrations_directory  )];
 has schema_migrations_table => sub { 'schema_migrations' };
@@ -132,9 +132,9 @@ sub _migration_key {
 1;
 __END__
 
-=head1 DB::SQL::Migrations
+=head1 NAME
 
-DB::SQL::Migrations - Perl extension to apply migrations from a directory
+DB::SQL::Migrations - apply database migrations via scripts in a directory
 
 =head1 SYNOPSIS
 
@@ -149,6 +149,10 @@ DB::SQL::Migrations - Perl extension to apply migrations from a directory
 =head1 DESCRIPTION
 
 Run a number of small SQL scripts
+
+=head1 REPOSITORY
+
+L<https://github.com/jontaylor/DB-SQL-Migrations>
 
 =head1 AUTHOR
 
